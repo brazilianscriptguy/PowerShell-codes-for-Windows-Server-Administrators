bb64c4707b947b43ced371b5d0b68ef9ee241651
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2023-12-24 08:53:23 -0300

    Add files via upload

---
+++ Eventlogs-Tools/Event ID 307 - Print Audit.ps1
@@ -0,0 +1,51 @@
+﻿# PowerShell Script for Processing Windows Event Log Files
+# Author: Luiz Hamilton Silva - @brazilianscriptguy
+# Update: 24/12/2023
+
+# Importing necessary assembly for OpenFileDialog
+Add-Type -AssemblyName System.Windows.Forms
+
+# Creating OpenFileDialog object with filter for .evtx files
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Filter = "Event Log files (*.evtx)|*.evtx"
+
+# Displaying OpenFileDialog and getting the file path
+if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $LogFilePath = $OpenFileDialog.FileName
+    $DefaultFolder = [Environment]::GetFolderPath("MyDocuments")
+    $timestamp = Get-Date -Format "yyyyMMddHHmmss"
+    $Destination = Join-Path $DefaultFolder "Evento307-Report_$timestamp.csv"
+
+    # Setting up COM objects for querying the Event Log
+    $LogQuery = New-Object -ComObject "MSUtil.LogQuery"
+    $InputFormat = New-Object -ComObject "MSUtil.LogQuery.EventLogInputFormat"
+    $OutputFormat = New-Object -ComObject "MSUtil.LogQuery.CSVOutputFormat"
+
+    # SQL-like query to extract specific fields from the event log
+    $SQLQuery = "SELECT timegenerated AS data_horario, Extract_token(strings, 2, '|') AS id_usuario, Extract_token(strings, 3, '|') AS estacao_trabalho, Extract_token(strings, 4, '|') AS impressora_utilizada, Extract_token(strings, 6, '|') AS tamanho_bytes, Extract_token(strings, 7, '|') AS quantidade_paginas_impressas INTO '" + $Destination + "' FROM '" + $LogFilePath + "' WHERE eventid = 307"
+
+    # Displaying the progress bar
+    Write-Progress -Activity "Processing Event Log" -Status "Please wait..." -PercentComplete 0
+
+    # Executing the query
+    $LogQuery.ExecuteBatch($SQLQuery, $InputFormat, $OutputFormat)
+
+    # Updating the progress bar to complete
+    Write-Progress -Activity "Processing Event Log" -Status "Completed" -PercentComplete 100
+    Start-Sleep -Seconds 2 # Display the completed status for a short duration
+
+    # Clearing the progress bar
+    Write-Progress -Activity "Processing Event Log" -Completed
+
+    # Releasing COM objects
+    $OutputFormat = $null
+    $InputFormat = $null
+    $LogQuery = $null
+
+    # Optionally open the generated CSV file
+    Start-Process $Destination
+} else {
+    Write-Host "No file selected."
+}
+
+#Script complet
