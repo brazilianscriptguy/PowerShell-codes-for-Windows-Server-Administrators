a003fc5bc84526a81173ce0079ec05c0be126220
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2023-12-24 10:34:27 -0300

    Update and rename EventID-4625-Account-failed-to-logon.ps1 to EventID-4625-Logon-account-failed.ps1

--- Eventlogs-Tools/EventID-4625-Logon-account-failed.ps1
+++ Eventlogs-Tools/EventID-4625-Logon-account-failed.ps1
@@ -18,7 +18,7 @@ if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
     $LogFilePath = $OpenFileDialog.FileName
     $DefaultFolder = [Environment]::GetFolderPath("MyDocuments")
     $timestamp = Get-Date -Format "yyyyMMddHHmmss"
-    $Destination = Join-Path $DefaultFolder "EventID-4625-FailedLogon_$timestamp.csv"
+    $Destination = Join-Path $DefaultFolder "EventID-4625-Logon-account-failed_$timestamp.csv"
 
     $LogQuery = New-Object -ComObject "MSUtil.LogQuery"
     $InputFormat = New-Object -ComObject "MSUtil.LogQuery.EventLogInputFormat"
