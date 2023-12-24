f23fe344480573de9f7a885499ecebc48ff10f5e
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2023-12-24 11:50:51 -0300

    Add files via upload

---
+++ Eventlogs-Tools/EventID-6008-System-shutsdown-unexpectedly.ps1
@@ -0,0 +1,50 @@
+# PowerShell Script for Monitoring Event ID 6008 - System Shuts Down Unexpectedly
+# Author: Luiz Hamilton Silva - @brazilianscriptguy
+# Update: 24/12/2023
+
+Param(
+    [Bool]$AutoOpen = $true
+)
+
+# Import necessary assembly for OpenFileDialog
+Add-Type -AssemblyName System.Windows.Forms
+
+# Creating OpenFileDialog object with filter for .evtx files
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Filter = "Event Log files (*.evtx)|*.evtx"
+
+# Displaying OpenFileDialog and getting the file path
+if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $LogFilePath = $OpenFileDialog.FileName
+    $DefaultFolder = [Environment]::GetFolderPath("MyDocuments")
+    $timestamp = Get-Date -Format "yyyyMMddHHmmss"
+    $Destination = Join-Path $DefaultFolder "EventID-6008-SystemShutdownUnexpectedly_$timestamp.csv"
+
+    $LogQuery = New-Object -ComObject "MSUtil.LogQuery"
+    $InputFormat = New-Object -ComObject "MSUtil.LogQuery.EventLogInputFormat"
+    $OutputFormat = New-Object -ComObject "MSUtil.LogQuery.CSVOutputFormat"
+
+    $SQLQuery = @"
+SELECT Count(*) AS Occurrences, 
+       eventid AS EventID 
+INTO '$Destination' 
+FROM '$LogFilePath' 
+WHERE eventid = 6008 
+GROUP BY EventID 
+ORDER BY Occurrences DESC
+"@
+    $LogQuery.ExecuteBatch($SQLQuery, $InputFormat, $OutputFormat)
+
+    if ($AutoOpen) {
+        try {
+            Start-Process $Destination
+        } catch {
+            Write-Host "Error: $($_.Exception.Message)" -ForegroundColor Red
+            Write-Host "No output file created if the query returned zero records!" -ForegroundColor Gray
+        } 
+    }
+} else {
+    Write-Host "No file selected."
+}
+
+#End of script
