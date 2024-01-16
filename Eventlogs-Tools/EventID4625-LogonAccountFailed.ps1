79586a3a9cf1582717f5014724cb2fdc06fe9f49
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-01-16 19:31:16 -0300

    Add files via upload
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

---
+++ Eventlogs-Tools/EventID4625-LogonAccountFailed.ps1
@@ -0,0 +1,54 @@
+# PowerShell Script for Processing Windows Event Log Files - Event ID 4625 (Failed AD Logon Attempts)
+# Author: Luiz Hamilton Silva - @brazilianscriptguy
+# Update: 24/12/2023
+
+Param(
+    [Bool]$AutoOpen = $false
+)
+
+# Import necessary assembly for OpenFileDialog
+Add-Type -AssemblyName System.Windows.Forms
+
+# Creating OpenFileDialog object with filter for .evtx files
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Filter = "Event Log files (*.evtx)|*.evtx"
+
+# Displaying OpenFileDialog and getting the file path
+if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $LogFilePath = $OpenFileDialog.FileName
+    $DefaultFolder = [Environment]::GetFolderPath("MyDocuments")
+    $timestamp = Get-Date -Format "yyyyMMddHHmmss"
+    $Destination = Join-Path $DefaultFolder "EventID-4625-Logon-account-failed_$timestamp.csv"
+
+    $LogQuery = New-Object -ComObject "MSUtil.LogQuery"
+    $InputFormat = New-Object -ComObject "MSUtil.LogQuery.EventLogInputFormat"
+    $OutputFormat = New-Object -ComObject "MSUtil.LogQuery.CSVOutputFormat"
+
+    $SQLQuery = @"
+SELECT timegenerated AS EventTime, 
+       Extract_token(strings, 5, '|') AS UserAccount, 
+       Extract_token(strings, 9, '|') AS SubStatusCode, 
+       Extract_token(strings, 10, '|') AS LogonType, 
+       Extract_token(strings, 13, '|') AS StationUser, 
+       Extract_token(strings, 19, '|') AS SourceIP
+INTO '$Destination' 
+FROM '$LogFilePath' 
+WHERE eventid = 4625 AND 
+      UserAccount NOT IN ('SYSTEM', 'ANONYMOUS LOGON', 'LOCAL SERVICE', 'NETWORK SERVICE') 
+ORDER BY EventTime DESC
+"@
+    $rtnVal = $LogQuery.ExecuteBatch($SQLQuery, $InputFormat, $OutputFormat)
+
+    if ($AutoOpen) {
+        try {
+            Start-Process $Destination
+        } catch {
+            Write-Host $_.Exception.Message -ForegroundColor Red
+            Write-Host "No output file created if the query returned zero records!" -ForegroundColor Gray
+        } 
+    }
+} else {
+    Write-Host "No file selected."
+}
+
+#End of script
---
+++ Eventlogs-Tools/EventID4771-KerberosPreAuthFailed.ps1
@@ -0,0 +1,50 @@
+# PowerShell Script for Processing Windows Event Log Files - Event ID 4771 (Kerberos Pre-Authentication Failed)
+# Author: Luiz Hamilton Silva - @brazilianscriptguy
+# Update: 24/12/2023
+
+Param(
+    [Bool]$AutoOpen = $false
+)
+
+# Import necessary assembly for OpenFileDialog
+Add-Type -AssemblyName System.Windows.Forms
+
+# Creating OpenFileDialog object with filter for .evtx files
+$OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+$OpenFileDialog.Filter = "Event Log files (*.evtx)|*.evtx"
+
+# Displaying OpenFileDialog and getting the file path
+if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+    $LogFilePath = $OpenFileDialog.FileName
+    $DefaultFolder = [Environment]::GetFolderPath("MyDocuments")
+    $timestamp = Get-Date -Format "yyyyMMddHHmmss"
+    $Destination = Join-Path $DefaultFolder "EventID-4771-KerberosPreAuthFailed_$timestamp.csv"
+
+    $LogQuery = New-Object -ComObject "MSUtil.LogQuery"
+    $InputFormat = New-Object -ComObject "MSUtil.LogQuery.EventLogInputFormat"
+    $OutputFormat = New-Object -ComObject "MSUtil.LogQuery.CSVOutputFormat"
+
+    $SQLQuery = @"
+SELECT timegenerated AS EventTime, 
+       Extract_token(strings, 0, '|') AS UserAccount, 
+       Extract_token(strings, 4, '|') AS LockoutCode, 
+       Extract_token(strings, 6, '|') AS StationIP
+INTO '$Destination' 
+FROM '$LogFilePath' 
+WHERE eventid = 4771
+"@
+    $rtnVal = $LogQuery.ExecuteBatch($SQLQuery, $InputFormat, $OutputFormat)
+
+    if ($AutoOpen) {
+        try {
+            Start-Process $Destination
+        } catch {
+            Write-Host $_.Exception.Message -ForegroundColor Red
+            Write-Host "No output file created if the query returned zero records!" -ForegroundColor Gray
+        } 
+    }
+} else {
+    Write-Host "No file selected."
+}
+
+#End of script
