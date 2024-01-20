d7e70a998c583bc1e110ed8f9ff8574fcb370633
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-01-20 18:13:05 -0300

    Add files via upload
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- AD-AdminTools/RemoteAccess-MultipleRDP.ps1
+++ AD-AdminTools/RemoteAccess-MultipleRDP.ps1
@@ -1,6 +1,6 @@
 ﻿# PowerShell Script for Multiple RDP Access with GUI
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: 12/01/2024
+# Update: 20/01/2024
 
 # Load Windows Forms and drawing libraries
 Add-Type -AssemblyName System.Windows.Forms
@@ -22,31 +22,45 @@ function Write-Log {
     Add-Content -Path $logFilePath -Value $message
 }
 
-# Function to create GUI for file path input
+# Function to create GUI for file path input and file browsing
 function Get-ServerListFilePath {
     while ($true) {
         # Create the form
         $form = New-Object System.Windows.Forms.Form
         $form.Text = 'Multiple RDP Access'
-        $form.Size = New-Object System.Drawing.Size(500,200) # Adjusted size
+        $form.Size = New-Object System.Drawing.Size(500,200)
         $form.StartPosition = 'CenterScreen'
 
         # Add the label
         $label = New-Object System.Windows.Forms.Label
         $label.Location = New-Object System.Drawing.Point(10,20)
-        $label.Size = New-Object System.Drawing.Size(460,40) # Adjusted size
-        $label.Text = 'Please enter the full path of a .TXT file (containing IPAddress or FQDN):'
+        $label.Size = New-Object System.Drawing.Size(480,20)
+        $label.Text = 'Please select a .TXT file (containing IPAddress or FQDN):'
         $form.Controls.Add($label)
 
         # Add the textbox
         $textBox = New-Object System.Windows.Forms.TextBox
-        $textBox.Location = New-Object System.Drawing.Point(10,70)
-        $textBox.Size = New-Object System.Drawing.Size(460,20) # Adjusted size
+        $textBox.Location = New-Object System.Drawing.Point(10,50)
+        $textBox.Size = New-Object System.Drawing.Size(360,20)
         $form.Controls.Add($textBox)
+
+        # Add the browse button
+        $browseButton = New-Object System.Windows.Forms.Button
+        $browseButton.Location = New-Object System.Drawing.Point(380,48)
+        $browseButton.Size = New-Object System.Drawing.Size(100,23)
+        $browseButton.Text = 'Browse'
+        $browseButton.Add_Click({
+            $openFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+            $openFileDialog.Filter = "Text Files (*.txt)|*.txt"
+            if ($openFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
+                $textBox.Text = $openFileDialog.FileName
+            }
+        })
+        $form.Controls.Add($browseButton)
 
         # Add the submit button
         $submitButton = New-Object System.Windows.Forms.Button
-        $submitButton.Location = New-Object System.Drawing.Point(395,120)
+        $submitButton.Location = New-Object System.Drawing.Point(205,100)
         $submitButton.Size = New-Object System.Drawing.Size(75,23)
         $submitButton.Text = 'Submit'
         $submitButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
@@ -92,18 +106,18 @@ while ($true) {
     }
 
     # Loop through the list of servers and start Remote Desktop sessions
-foreach ($server in $servers) {
-    if ([string]::IsNullOrWhiteSpace($server)) {
-        Write-Log "Skipped empty or whitespace server entry"
-        continue
+    foreach ($server in $servers) {
+        if ([string]::IsNullOrWhiteSpace($server)) {
+            Write-Log "Skipped empty or whitespace server entry"
+            continue
+        }
+
+        Write-Log "Initiating RDP session to: $server"
+        Start-Process -FilePath "mstsc" -ArgumentList "/v:$server" -Wait
     }
 
-    Write-Log "Initiating RDP session to: $server"
-    Start-Process -FilePath "mstsc" -ArgumentList "/v:$server" -Wait
-}
-
-# End of the main loop
-break
+    # End of the main loop
+    break
 }
 
 #End of script
