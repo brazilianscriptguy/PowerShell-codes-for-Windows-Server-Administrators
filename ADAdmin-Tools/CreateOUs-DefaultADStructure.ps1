1fb47aae040e5d2ad45e2a0fcf61b334fe00462f
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-04 21:14:10 -0300

    Add files via upload
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/CreateOUs-DefaultADStructure.ps1
+++ ADAdmin-Tools/CreateOUs-DefaultADStructure.ps1
@@ -6,6 +6,28 @@
 Add-Type -AssemblyName System.Windows.Forms
 Import-Module ActiveDirectory
 
+# Determine the script name and set up logging path
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+$logDir = 'C:\Logs-TEMP'
+$logFileName = "${scriptName}.log"
+$logPath = Join-Path $logDir $logFileName
+
+# Ensure the log directory exists
+if (-not (Test-Path $logDir)) {
+    New-Item -Path $logDir -ItemType Directory | Out-Null
+}
+
+# Logging function
+function Log-Message {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "[$timestamp] $Message"
+    Add-Content -Path $logPath -Value $logEntry
+}
+
 # Create and configure the main form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Create Specified Organizational Units'
@@ -46,10 +68,13 @@ $buttonCreateOU.Size = New-Object System.Drawing.Size(150, 23)
 $buttonCreateOU.Add_Click({
     $destinationOU = $textBoxDestinationOU.Text
     $ouNames = $textBoxOUNames.Text -split ',' | ForEach-Object { $_.Trim() }
+
+    Log-Message "Attempting to create OUs in destination: $destinationOU with names: $($ouNames -join ', ')"
     
     # Validation
     if([string]::IsNullOrWhiteSpace($destinationOU) -or $ouNames.Length -eq 0) {
         [System.Windows.Forms.MessageBox]::Show("Please enter the Destination OU and OU names.", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+        Log-Message "Validation failed: Destination OU or OU names not entered."
         return
     }
 
@@ -57,16 +82,19 @@ $buttonCreateOU.Add_Click({
     foreach ($ouName in $ouNames) {
         try {
             New-ADOrganizationalUnit -Name $ouName -Path $destinationOU -ProtectedFromAccidentalDeletion $false
+            Log-Message "Successfully created OU: $ouName"
         } catch {
+            Log-Message "Error occurred creating '$ouName': $_"
             [System.Windows.Forms.MessageBox]::Show("An error occurred creating '$ouName': $_", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
             return
         }
     }
     [System.Windows.Forms.MessageBox]::Show("Specified OUs created successfully in '$destinationOU'.", "Success", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
+    Log-Message "Specified OUs created successfully in '$destinationOU'."
 })
 $form.Controls.Add($buttonCreateOU)
 
 # Show the form
 $form.ShowDialog() | Out-Null
 
-#End of script
+# End of script
