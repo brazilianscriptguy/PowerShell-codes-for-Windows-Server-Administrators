6bb8332d09c18f4ce8532ab583f7a44f8cf1120a
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-07 14:34:58 -0300

    Update EventID4800and4801-WorkstationLockStatus.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- Eventlogs-Tools/EventID4800and4801-WorkstationLockStatus.ps1
+++ Eventlogs-Tools/EventID4800and4801-WorkstationLockStatus.ps1
@@ -1,6 +1,6 @@
 ﻿# PowerShell Script for Processing Windows Event Log Files - Event ID 4800 and 4801 (Locked and Unlocked Workstation)
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: 04/03/2024
+# Updated: April 7, 2024
 
 Param(
     [Bool]$AutoOpen = $true
