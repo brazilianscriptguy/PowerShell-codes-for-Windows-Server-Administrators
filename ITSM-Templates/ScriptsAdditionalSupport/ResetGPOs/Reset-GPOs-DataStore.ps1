cdebbcfc0e9d609d465724a29e26eb8f24eb223f
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-09 18:44:40 -0300

    Update Reset-GPOs-DataStore.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ITSM-Templates/ScriptsAdditionalSupport/ResetGPOs/Reset-GPOs-DataStore.ps1
+++ ITSM-Templates/ScriptsAdditionalSupport/ResetGPOs/Reset-GPOs-DataStore.ps1
@@ -1,20 +1,20 @@
 ﻿# PowerShell Script to Reset and Clear Current GPOs of the Workstation and Perform New Synchronization with the Forest
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: March, 29, 2024
+# Update: April 9, 2024.
 
-# Function to delete directories where GPOs are stored
-function Delete-GPODirectory {
+# Function to delete directories where Group Policy Objects (GPOs) are stored
+function Remove-GPODirectory {
     param (
         [string]$DirectoryPath
     )
 
     if (Test-Path -Path $DirectoryPath) {
         Remove-Item -Path $DirectoryPath -Recurse -Force -ErrorAction SilentlyContinue
-        Log-Message "GPO directory deleted: $DirectoryPath"
+        Write-Output "GPO directory deleted: $DirectoryPath"
     }
 }
 
-# Log function
+# Logging function
 function Log-Message {
     param (
         [string]$Message
@@ -29,25 +29,33 @@ try {
     # Logging the start of script execution
     Log-Message "Script execution started."
 
+    # Resetting GPOs using "setup security.inf"
+    secedit /configure /db reset.sdb /cfg "C:\Windows\security\templates\setup security.inf" /overwrite /quiet
+    Log-Message "GPOs reset using setup security.inf"
+
+    # Resetting GPOs using "defltbase.inf"
+    secedit /configure /db reset.sdb /cfg "$env:windir\inf\defltbase.inf" /areas USER_POLICY, MACHINE_POLICY, SECURITYPOLICY /overwrite /quiet
+    Log-Message "GPOs reset using defltbase.inf"
+
     # Deleting the registry key where current GPO settings reside
     Remove-Item -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Group Policy" -Recurse -Force -ErrorAction SilentlyContinue
-    Log-Message "Registry key for GPOs deleted."
+    Log-Message "GPO registry key deleted."
 
     # Removing Group Policy directories
-    $envWinDir = [System.Environment]::GetEnvironmentVariable("WinDir")
-    Delete-GPODirectory -DirectoryPath "$envWinDir\System32\GroupPolicy"
-    Delete-GPODirectory -DirectoryPath "$envWinDir\System32\GroupPolicyUsers"
-    Delete-GPODirectory -DirectoryPath "$envWinDir\SysWOW64\GroupPolicy"
-    Delete-GPODirectory -DirectoryPath "$envWinDir\SysWOW64\GroupPolicyUsers"
+    Remove-GPODirectory -DirectoryPath "$env:windir\System32\GroupPolicy"
+    Remove-GPODirectory -DirectoryPath "$env:windir\System32\GroupPolicyUsers"
+    Remove-GPODirectory -DirectoryPath "$env:windir\SysWOW64\GroupPolicy"
+    Remove-GPODirectory -DirectoryPath "$env:windir\SysWOW64\GroupPolicyUsers"
 
     # Logging the successful completion of script execution
     Log-Message "Script execution completed successfully."
 
-    # Scheduling system reboot after 15 seconds
-    Start-Process "shutdown" -ArgumentList "/r /f /t 15 /c ""System will restart in 15 seconds for GPO resynchronization!""" -NoNewWindow -Wait
+    # Scheduling system restart after 15 seconds
+    Start-Sleep -Seconds 15
+    Restart-Computer -Force
 }
 catch {
-    # Logging any error that occurs during script execution
+    # Logging any errors that occur during script execution
     Log-Message "An error occurred: $_"
 }
 
