0bc870649f78891c3f01330066dacf03f2c4d174
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-11 16:33:09 -0300

    Update Unlock-All-AdminShares.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ITSM-Templates/ScriptsAdditionalSupport/UnlockAllAdminShares/Unlock-All-AdminShares.ps1
+++ ITSM-Templates/ScriptsAdditionalSupport/UnlockAllAdminShares/Unlock-All-AdminShares.ps1
@@ -1,59 +1,64 @@
 # PowerShell Script to UNLOCK ALL ADMINISTRATIVE SHARES, ACTIVATE RDP, TURN OFF WINDOWS FIREWALL, AND DISABLE WINDOWS DEFENDER
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: April 9, 2024.
+# Update: April 11, 2024.
 
-# Log file path
-$logPath = "c:\ITSM-Logs\Unlock-All-AdminShares.log"
+# Determines the script name and sets up the log path
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+$logDir = 'C:\ITSM-Logs'
+$logFileName = "${scriptName}.log"
+$logPath = Join-Path $logDir $logFileName
 
-# Check and create ITSM-Logs folder if it does not exist
-$logFolder = "c:\ITSM-Logs"
-If (-Not (Test-Path $logFolder)) {
-    New-Item -ItemType Directory -Path $logFolder
-    "Log folder $logFolder created." | Out-File -FilePath $logPath
+# Ensures the log directory exists
+if (-not (Test-Path $logDir)) {
+    New-Item -Path $logDir -ItemType Directory | Out-Null
 }
 
-# Function to add log
-function Write-Log {
-    Param ([string]$message)
-    $timeStamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    "$timeStamp - $message" | Out-File -FilePath $logPath -Append
+# Logging function
+function Log-Message {
+    param (
+        [Parameter(Mandatory=$true)]
+        [string]$Message
+    )
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "[$timestamp] $Message"
+    Add-Content -Path $logPath -Value $logEntry
 }
 
-# Activate Administrative Shares
+# Enables Administrative Shares
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Services\LanManServer\Parameters" -Name AutoShareWks -Value 1 -Type DWord
-Write-Log "Administrative shares activated."
+Log-Message "Administrative shares successfully enabled."
 
-# Wait for the registry change to take effect
+# Waits for the registry change to take effect
 Start-Sleep -Seconds 2
 
-# Enable Remote Desktop and Disable NLA
+# Enables Remote Desktop and Disables NLA
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name fDenyTSConnections -Value 0 -Type DWord
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name UserAuthentication -Value 0 -Type DWord
-Write-Log "Remote Desktop enabled and Network Level Authentication (NLA) disabled."
+Log-Message "Remote Desktop enabled and Network Level Authentication (NLA) disabled."
 
-# Ensure RDP is enabled in Windows Settings
+# Ensures that RDP is enabled in Windows Settings
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server" -Name fAllowToGetHelp -Value 1 -Type DWord
-Write-Log "RDP enabled in Windows Settings."
+Log-Message "RDP enabled in Windows Settings."
 
-# Configure Windows Firewall to allow RDP
+# Configures Windows Firewall to allow RDP
 Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
-Write-Log "Firewall configured to allow RDP."
+Log-Message "Firewall configured to allow RDP."
 
-# Disable Windows Firewall for all profiles
+# Disables Windows Firewall for all profiles
 Set-NetFirewallProfile -All -Enabled False
-Write-Log "Windows Firewall disabled for all profiles."
+Log-Message "Windows Firewall disabled for all profiles."
 
-# Disable Windows Defender
+# Disables Windows Defender
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name DisableAntiSpyware -Value 1 -Type DWord
-Write-Log "Windows Defender disabled."
+Log-Message "Windows Defender disabled."
 
-# Check and enable listening on port 3389 for RDP
+# Checks and enables listening on port 3389 for RDP
 Set-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp" -Name PortNumber -Value 3389 -Type DWord
-Write-Log "Port 3389 configured for RDP listening."
+Log-Message "Port 3389 configured for RDP listening."
 
-# Notify completion
-$completionMessage = "Script execution completed. Administrative shares, RDP, and firewall settings have been updated. All RDP access barriers have been removed."
-Write-Log $completionMessage
+# Notification of completion
+$completionMessage = "Script execution completed. Administrative shares, RDP, and firewall settings updated. All RDP access barriers have been removed."
+Log-Message $completionMessage
 Write-Output $completionMessage
 
 # End of Script
