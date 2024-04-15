bbd80bd026d9cb2765ebf5b33745812af22d7c55
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-15 16:16:27 -0300

    Update CreateOUs-DefaultADStructure.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/CreateOUs-DefaultADStructure.ps1
+++ ADAdmin-Tools/CreateOUs-DefaultADStructure.ps1
@@ -1,6 +1,6 @@
 # PowerShell Script with GUI for Creating Specified OUs in Active Directory
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: March, 04, 2024
+# Update: April 15, 2024.
 
 # Import necessary modules
 Add-Type -AssemblyName System.Windows.Forms
