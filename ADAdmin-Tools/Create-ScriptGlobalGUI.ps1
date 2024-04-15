43c90755306cfd5da7c4bf2b40ceb53e491f1821
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-15 16:06:48 -0300

    Update Create-ScriptGlobalGUI.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/Create-ScriptGlobalGUI.ps1
+++ ADAdmin-Tools/Create-ScriptGlobalGUI.ps1
@@ -1,6 +1,6 @@
- # PowerShell script to server as model, template and defaul for all GUIs into new scripts
+# PowerShell script to server as model, template and defaul for all GUIs into new scripts - It's call a Windows Credentials test
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: March, 04, 2024
+# Update: April 15, 2024.
  
  # Function to create the Default GUI for My Scripts
 function Create-GUI {
