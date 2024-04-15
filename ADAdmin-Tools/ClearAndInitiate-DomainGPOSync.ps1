e5848a52428465346fc1d20297ac36c81ab4a7f0
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-15 16:10:50 -0300

    Update ClearAndInitiate-DomainGPOSync.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/ClearAndInitiate-DomainGPOSync.ps1
+++ ADAdmin-Tools/ClearAndInitiate-DomainGPOSync.ps1
@@ -10,19 +10,36 @@ function Remove-GPODirectory {
 
     if (Test-Path -Path $DirectoryPath) {
         Remove-Item -Path $DirectoryPath -Recurse -Force -ErrorAction SilentlyContinue
-        Write-Output "GPO directory deleted: $DirectoryPath"
+        Log-Message "GPO directory deleted: $DirectoryPath"
     }
 }
 
-# Logging function
+# Determine the script name and set up logging path
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+$logDir = 'C:\Logs-TEMP'
+$logFileName = "${scriptName}.log"
+$logPath = Join-Path $logDir $logFileName
+
+# Ensure the log directory exists
+if (-not (Test-Path $logDir)) {
+    New-Item -Path $logDir -ItemType Directory -ErrorAction Stop | Out-Null
+    Log-Message "Log directory $logDir created."
+}
+
+# Updated logging function
 function Log-Message {
     param (
+        [Parameter(Mandatory=$true)]
         [string]$Message
     )
 
-    $LogEntry = "$(Get-Date -Format 'yyyy-MM-dd HH:mm:ss') - $Message"
-    $LogFilePath = "C:\Logs-TEMP\ClearAndInitiate-DomainGPOSync.log"
-    Add-Content -Path $LogFilePath -Value $LogEntry
+    $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
+    $logEntry = "[$timestamp] - $Message"
+    try {
+        Add-Content -Path $logPath -Value $logEntry -ErrorAction Stop
+    } catch {
+        Write-Error "Failed to log to $logPath. Error: $_"
+    }
 }
 
 try {
