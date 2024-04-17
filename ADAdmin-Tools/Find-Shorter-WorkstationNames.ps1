237b9ce879920030cbe0e79982d92b839fcbddaa
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-04-17 13:41:46 -0300

    Create Find-Shorter-WorkstationNames.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

---
+++ ADAdmin-Tools/Find-Shorter-WorkstationNames.ps1
@@ -0,0 +1,88 @@
+# PowerShell script to search Active Directory for workstation computers with names shorter than 15 characters
+# Author: Luiz Hamilton Silva - luizhamilton.lhr@gmail.com
+# Update: April 17, 2024. 
+
+# Add necessary assemblies for GUI
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+
+# Function to search Active Directory for workstation computers with names shorter than 15 characters
+function Search-ADComputers {
+    Import-Module ActiveDirectory
+    $osFilter = "OperatingSystem -notlike '*Server*'"
+    $server = $txtServer.Text
+
+    # Use the -Server parameter directly with Get-ADComputer if a server name is provided
+    $computers = Get-ADComputer -Filter "($osFilter) -and (Name -like '*')" -Property Name, DNSHostName -Server $server | Where-Object { $_.Name.Length -lt 15 }
+    $listBox.Items.Clear()
+    $results = @()
+
+    foreach ($comp in $computers) {
+        $obj = New-Object PSObject -Property @{
+            CharactersLength = $comp.Name.Length
+            WorkstationFQDN = $comp.DNSHostName
+        }
+        $results += $obj
+        $listBox.Items.Add($comp.DNSHostName)
+    }
+    
+    # Store results in a global variable for later use in CSV export
+    $global:exportData = $results
+}
+
+# Determine the script name for logging and exporting
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
+# Main Form
+$mainForm = New-Object System.Windows.Forms.Form
+$mainForm.Text = 'Search ADDS Workstations'
+$mainForm.Size = New-Object System.Drawing.Size(400,350)
+$mainForm.StartPosition = 'CenterScreen'
+
+# Server Name Label
+$lblServer = New-Object System.Windows.Forms.Label
+$lblServer.Location = New-Object System.Drawing.Point(30,30)
+$lblServer.Size = New-Object System.Drawing.Size(120,20)
+$lblServer.Text = 'AD Server Name:'
+$mainForm.Controls.Add($lblServer)
+
+# Server Name Text Box
+$txtServer = New-Object System.Windows.Forms.TextBox
+$txtServer.Location = New-Object System.Drawing.Point(150,30)
+$txtServer.Size = New-Object System.Drawing.Size(200,20)
+$mainForm.Controls.Add($txtServer)
+
+# Search Button
+$btnSearch = New-Object System.Windows.Forms.Button
+$btnSearch.Location = New-Object System.Drawing.Point(30,60)
+$btnSearch.Size = New-Object System.Drawing.Size(100,23)
+$btnSearch.Text = 'Search'
+$btnSearch.Add_Click({
+    Search-ADComputers
+})
+$mainForm.Controls.Add($btnSearch)
+
+# List Box to display results
+$listBox = New-Object System.Windows.Forms.ListBox
+$listBox.Location = New-Object System.Drawing.Point(30,90)
+$listBox.Size = New-Object System.Drawing.Size(320,150)
+$mainForm.Controls.Add($listBox)
+
+# Export Button
+$btnExport = New-Object System.Windows.Forms.Button
+$btnExport.Location = New-Object System.Drawing.Point(30,250)
+$btnExport.Size = New-Object System.Drawing.Size(100,23)
+$btnExport.Text = 'Export to CSV file'
+$btnExport.Add_Click({
+    $dcName = if ($txtServer.Text) { $txtServer.Text } else { "Local" }
+    $timestamp = Get-Date -Format "yyyyMMdd-HHmmss"
+    $csvPath = [Environment]::GetFolderPath('MyDocuments') + "\${scriptName}-$dcName-$timestamp.csv"
+    $global:exportData | Select-Object CharactersLength, WorkstationFQDN | Export-Csv -Path $csvPath -NoTypeInformation -Delimiter ';' -Encoding UTF8
+    [System.Windows.Forms.MessageBox]::Show("Data exported to $csvPath")
+})
+$mainForm.Controls.Add($btnExport)
+
+# Show GUI
+$mainForm.ShowDialog()
+
+# End of script
