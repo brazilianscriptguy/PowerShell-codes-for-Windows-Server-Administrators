be096de8c39c8875a054dc86f0fab4b0c2289744
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-05-06 19:24:17 -0300

    Update List-UsersWithNonExpiringPasswords.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/List-UsersWithNonExpiringPasswords.ps1
+++ ADAdmin-Tools/List-UsersWithNonExpiringPasswords.ps1
@@ -1,4 +1,4 @@
-﻿# PowerShell Script to Export List of AD Users with Non-Expiring Passwords to CSV with GUI
+# PowerShell Script to Export List of AD Users with Non-Expiring Passwords to CSV with GUI
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
 # Update: May 06, 2024.
 
@@ -11,49 +11,52 @@ if (-not (Get-Module -ListAvailable -Name ActiveDirectory)) {
     return
 }
 
+# Determine the script name and set up the path for the CSV export
+$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+
 # Create main form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Export AD Users with Non-Expiring Passwords'
-$form.Size = New-Object System.Drawing.Size(400,200)
+$form.Size = New-Object System.Drawing.Size(400, 200)
 $form.StartPosition = 'CenterScreen'
 
 # Domain FQDN label and textbox
 $labelDomain = New-Object System.Windows.Forms.Label
-$labelDomain.Location = New-Object System.Drawing.Point(10,20)
-$labelDomain.Size = New-Object System.Drawing.Size(380,20)
+$labelDomain.Location = New-Object System.Drawing.Point(10, 20)
+$labelDomain.Size = New-Object System.Drawing.Size(380, 20)
 $labelDomain.Text = 'Enter the FQDN of the Domain:'
 $form.Controls.Add($labelDomain)
 
 $textboxDomain = New-Object System.Windows.Forms.TextBox
-$textboxDomain.Location = New-Object System.Drawing.Point(10,40)
-$textboxDomain.Size = New-Object System.Drawing.Size(360,20)
+$textboxDomain.Location = New-Object System.Drawing.Point(10, 40)
+$textboxDomain.Size = New-Object System.Drawing.Size(360, 20)
 $form.Controls.Add($textboxDomain)
 
 # Export button
 $exportButton = New-Object System.Windows.Forms.Button
-$exportButton.Location = New-Object System.Drawing.Point(10,80)
-$exportButton.Size = New-Object System.Drawing.Size(360,30)
+$exportButton.Location = New-Object System.Drawing.Point(10, 80)
+$exportButton.Size = New-Object System.Drawing.Size(360, 30)
 $exportButton.Text = 'Export to CSV'
 $exportButton.Add_Click({
     $domainFQDN = $textboxDomain.Text
     if (![string]::IsNullOrWhiteSpace($domainFQDN)) {
         try {
             $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
-            $outputFile = "$([Environment]::GetFolderPath('MyDocuments'))\List-UsersWithNonExpiringPasswords_${domainFQDN}_$timestamp.csv"
+            $csvPath = [Environment]::GetFolderPath('MyDocuments') + "\${scriptName}-${domainFQDN}-${timestamp}.csv"
             
             $neverExpireUsers = Get-ADUser -Filter { PasswordNeverExpires -eq $true } -Properties PasswordNeverExpires -Server $domainFQDN |
-                               Select-Object Name, SamAccountName, DistinguishedName
+                                Select-Object Name, SamAccountName, DistinguishedName
             if ($neverExpireUsers.Count -gt 0) {
-                $neverExpireUsers | Export-Csv -Path $outputFile -NoTypeInformation
-                [System.Windows.Forms.MessageBox]::Show("Report exported to: `n$outputFile", "Export Successful")
+                $neverExpireUsers | Export-Csv -Path $csvPath -NoTypeInformation
+                [System.Windows.Forms.MessageBox]::Show("Report exported to: `n$csvPath", "Export Successful", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
             } else {
-                [System.Windows.Forms.MessageBox]::Show("No users with 'Password Never Expires' found in $domainFQDN.", "No Data Found")
+                [System.Windows.Forms.MessageBox]::Show("No users with 'Password Never Expires' found in $domainFQDN.", "No Data Found", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
             }
         } catch {
-            [System.Windows.Forms.MessageBox]::Show("An error occurred: $_", "Error")
+            [System.Windows.Forms.MessageBox]::Show("An error occurred: $_", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
         }
     } else {
-        [System.Windows.Forms.MessageBox]::Show('Please enter a valid FQDN of the Domain.', 'Input Error')
+        [System.Windows.Forms.MessageBox]::Show('Please enter a valid FQDN of the Domain.', 'Input Error', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
     }
 })
 $form.Controls.Add($exportButton)
