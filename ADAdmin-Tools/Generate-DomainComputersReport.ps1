340606a6555676f25ee7803e30a336e7aa545951
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-05-06 18:02:54 -0300

    Update Generate-DomainComputersReport.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/Generate-DomainComputersReport.ps1
+++ ADAdmin-Tools/Generate-DomainComputersReport.ps1
@@ -1,32 +1,45 @@
-﻿# PowerShell Script to Generate Report of Computers in Specified Domain
+# PowerShell Script to Generate Report of Computers in Specified Domain
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: March, 04, 2024
+# Update: May 06, 2024
 
 Add-Type -AssemblyName System.Windows.Forms
 Import-Module ActiveDirectory
 
+# Function to get the FQDN of the domain name and forest name
+function Get-DomainFQDN {
+    try {
+        $ComputerSystem = Get-WmiObject Win32_ComputerSystem
+        $Domain = $ComputerSystem.Domain
+        return $Domain
+    } catch {
+        Write-Warning "Unable to fetch FQDN automatically."
+        return "YourDomainHere"
+    }
+}
+
 # Create main form
 $form = New-Object System.Windows.Forms.Form
 $form.Text = 'Report of Domain Computers'
-$form.Size = New-Object System.Drawing.Size(400,200)
+$form.Size = New-Object System.Drawing.Size(400, 200)
 $form.StartPosition = 'CenterScreen'
 
 # Domain FQDN label and textbox
 $labelDomain = New-Object System.Windows.Forms.Label
-$labelDomain.Location = New-Object System.Drawing.Point(10,20)
-$labelDomain.Size = New-Object System.Drawing.Size(380,20)
+$labelDomain.Location = New-Object System.Drawing.Point(10, 20)
+$labelDomain.Size = New-Object System.Drawing.Size(380, 20)
 $labelDomain.Text = 'Enter the FQDN of the Domain:'
 $form.Controls.Add($labelDomain)
 
 $textboxDomain = New-Object System.Windows.Forms.TextBox
-$textboxDomain.Location = New-Object System.Drawing.Point(10,40)
-$textboxDomain.Size = New-Object System.Drawing.Size(360,20)
+$textboxDomain.Location = New-Object System.Drawing.Point(10, 40)
+$textboxDomain.Size = New-Object System.Drawing.Size(360, 20)
+$textboxDomain.Text = Get-DomainFQDN
 $form.Controls.Add($textboxDomain)
 
 # Generate report button
 $generateButton = New-Object System.Windows.Forms.Button
-$generateButton.Location = New-Object System.Drawing.Point(10,80)
-$generateButton.Size = New-Object System.Drawing.Size(360,30)
+$generateButton.Location = New-Object System.Drawing.Point(10, 80)
+$generateButton.Size = New-Object System.Drawing.Size(170, 30)
 $generateButton.Text = 'Generate Report'
 $generateButton.Add_Click({
     $domainFQDN = $textboxDomain.Text
@@ -34,7 +47,8 @@ $generateButton.Add_Click({
         try {
             $timestamp = Get-Date -Format "yyyyMMdd_HHmmss"
             $sanitizedDomainFQDN = $domainFQDN -replace "\.", "_"
-            $outputFile = "$([Environment]::GetFolderPath('MyDocuments'))\Generate-DomainComputersReport_${sanitizedDomainFQDN}_$timestamp.csv"
+            $scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
+            $outputFile = "$([Environment]::GetFolderPath('MyDocuments'))\${scriptName}_${sanitizedDomainFQDN}_$timestamp.csv"
 
             $domainControllers = Get-ADComputer -Filter { (OperatingSystem -Like '*Server*') -and (IsDomainController -eq $true) } -Server $domainFQDN -Properties Name, OperatingSystem, OperatingSystemVersion | Select-Object Name, OperatingSystem, OperatingSystemVersion
             $domainComputers = Get-ADComputer -Filter { OperatingSystem -NotLike '*Server*' } -Server $domainFQDN -Properties Name, OperatingSystem, OperatingSystemVersion | Select-Object Name, OperatingSystem, OperatingSystemVersion
@@ -44,13 +58,23 @@ $generateButton.Add_Click({
 
             [System.Windows.Forms.MessageBox]::Show("Computers exported to `n$outputFile", "Export Successful", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Information)
         } catch {
-            [System.Windows.Forms.MessageBox]::Show("Error querying Active Directory: $_", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
+            [System.Windows.Forms.MessageBox]::Show("Error querying Active Directory: $($_.Exception.Message)", "Error", [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Error)
         }
     } else {
         [System.Windows.Forms.MessageBox]::Show('Please enter a valid FQDN of the Domain.', 'Input Error', [System.Windows.Forms.MessageBoxButtons]::OK, [System.Windows.Forms.MessageBoxIcon]::Warning)
     }
 })
 $form.Controls.Add($generateButton)
+
+# Close button
+$closeButton = New-Object System.Windows.Forms.Button
+$closeButton.Location = New-Object System.Drawing.Point(200, 80)
+$closeButton.Size = New-Object System.Drawing.Size(170, 30)
+$closeButton.Text = 'Close'
+$closeButton.Add_Click({
+    $form.Close()
+})
+$form.Controls.Add($closeButton)
 
 $form.ShowDialog()
 
