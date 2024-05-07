fe2a8a370d3a8513c1a7f40adffc2b7a30a3e129
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-05-07 10:52:34 -0300

    Update EventID307-PrintAudit.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- Eventlogs-Tools/EventID307-PrintAudit.ps1
+++ Eventlogs-Tools/EventID307-PrintAudit.ps1
@@ -1,51 +1,95 @@
 # PowerShell Script for Processing Windows Event Log Files - Event Microsoft-Windows-PrintService/Operational
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Updated: April 7, 2024
+# Updated: May 7, 2024.
 
-# Importing necessary assembly for OpenFileDialog
+# Hide the PowerShell console window
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Window {
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr GetConsoleWindow();
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    public static void Hide() {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 0); // 0 = SW_HIDE
+    }
+    public static void Show() {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 5); // 5 = SW_SHOW
+    }
+}
+"@
+
+[Window]::Hide()
+
+
+# Import necessary assembly for OpenFileDialog
 Add-Type -AssemblyName System.Windows.Forms
 
-# Creating OpenFileDialog object with filter for .evtx files
+# Create OpenFileDialog object
 $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
-$OpenFileDialog.Filter = "Event Log files (*.evtx)|*.evtx"
+$OpenFileDialog.filter = "Event Log files (*.evtx)|*.evtx"
 
-# Displaying OpenFileDialog and getting the file path
-if ($OpenFileDialog.ShowDialog() -eq [System.Windows.Forms.DialogResult]::OK) {
-    $LogFilePath = $OpenFileDialog.FileName
-    $DefaultFolder = [Environment]::GetFolderPath("MyDocuments")
+# Show OpenFileDialog
+$OpenFileDialog.ShowDialog() | Out-Null
+
+# Get the selected .evtx file path
+$LogFilePath = $OpenFileDialog.FileName
+
+# Check if the log file path is not empty
+if (![string]::IsNullOrWhiteSpace($LogFilePath)) {
+    # Your script starts here
+    $Error.Clear()
+    $DefaultFolder=[Environment]::GetFolderPath("MyDocuments")
     $timestamp = Get-Date -Format "yyyyMMddHHmmss"
-    $Destination = Join-Path $DefaultFolder "EventID307-PrintAudit_$timestamp.csv"
+    $Destination = "Evento307-Relatorio_$timestamp.csv"
+    $Destination = $DefaultFolder + "\" + $Destination
 
-    # Setting up COM objects for querying the Event Log
     $LogQuery = New-Object -ComObject "MSUtil.LogQuery"
     $InputFormat = New-Object -ComObject "MSUtil.LogQuery.EventLogInputFormat"
     $OutputFormat = New-Object -ComObject "MSUtil.LogQuery.CSVOutputFormat"
 
-    # SQL-like query to extract specific fields from the event log
-    $SQLQuery = "SELECT timegenerated AS EventTime, Extract_token(strings, 2, '|') AS UserId, Extract_token(strings, 3, '|') AS Workstation, Extract_token(strings, 4, '|') AS PrinterUsed, Extract_token(strings, 6, '|') AS ByteSize, Extract_token(strings, 7, '|') AS PagesPrinted INTO '" + $Destination + "' FROM '" + $LogFilePath + "' WHERE eventid = 307"
+    # ... Rest of your script
 
-    # Displaying the progress bar
-    Write-Progress -Activity "Processing Event Log" -Status "Please wait..." -PercentComplete 0
+    # Create a progress bar
+    $progressBar = @{
+        Activity = "Processing file..."
+        Status = "Please wait..."
+        PercentComplete = 0
+    }
+    Write-Progress @progressBar
 
-    # Executing the query
-    $LogQuery.ExecuteBatch($SQLQuery, $InputFormat, $OutputFormat)
+    $SQLQuery = "SELECT timegenerated AS data_horario, Extract_token(strings, 2, '|') AS id_usuario, Extract_token(strings, 3, '|') AS estacao_trabalho, Extract_token(strings, 4, '|') AS impressora_utilizada, Extract_token(strings, 6, '|') AS tamanho_bytes, Extract_token(strings, 7, '|') AS quantidade_paginas_impressas INTO '" + $Destination + "' FROM '" + $LogFilePath + "' WHERE eventid = 307"
 
-    # Updating the progress bar to complete
-    Write-Progress -Activity "Processing Event Log" -Status "Completed" -PercentComplete 100
-    Start-Sleep -Seconds 2 # Display the completed status for a short duration
+    $rtnVal = $LogQuery.ExecuteBatch($SQLQuery, $InputFormat, $OutputFormat);
 
-    # Clearing the progress bar
-    Write-Progress -Activity "Processing Event Log" -Completed
+    # Update the progress bar to complete
+    $progressBar.PercentComplete = 100
+    Write-Progress @progressBar
 
-    # Releasing COM objects
-    $OutputFormat = $null
-    $InputFormat = $null
-    $LogQuery = $null
+    $OutputFormat = $null;
+    $InputFormat = $null;
+    $LogQuery = $null;
 
-    # Optionally open the generated CSV file
-    Start-Process $Destination
-} else {
+    if($AutoOpen)
+    {
+        try
+        {
+            Start-Process($Destination)
+        }
+        catch
+        {
+            Write-Host $_.Exception.Message  -ForegroundColor Red
+            Write-Host $_.Exception.GetType().FullName  -ForegroundColor Red
+            Write-Host "NOTE: No output file will be created if the query returned zero records!"  -ForegroundColor Gray
+        }   
+    }
+}
+else {
     Write-Host "No file selected."
 }
 
-#End of script
+# End of script
