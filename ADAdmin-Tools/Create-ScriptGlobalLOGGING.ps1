160063adead0a7a090319e664c4e6bff051d2cf4
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-05-08 15:23:04 -0300

    Update Create-ScriptGlobalLOGGING.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/Create-ScriptGlobalLOGGING.ps1
+++ ADAdmin-Tools/Create-ScriptGlobalLOGGING.ps1
@@ -1,6 +1,34 @@
 # PowerShell script template serves as a standardized approach for setting up logging in PowerShell scripts.
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: April 15, 2024.
+# Update: May 07, 2024.
+
+# Hide the PowerShell console window
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Window {
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr GetConsoleWindow();
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    public static void Hide() {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 0); // 0 = SW_HIDE
+    }
+    public static void Show() {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 5); // 5 = SW_SHOW
+    }
+}
+"@
+
+[Window]::Hide()
+
+# Import necessary assemblies and modules
+Add-Type -AssemblyName System.Windows.Forms
+Add-Type -AssemblyName System.Drawing
+# Import-Module ActiveDirectory - Set it if will be necessary 
 
 # Determine the script name and set up logging path
 $scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
@@ -10,36 +38,17 @@ $logPath = Join-Path $logDir $logFileName
 
 # Ensure the log directory exists
 if (-not (Test-Path $logDir)) {
-    $null = New-Item -Path $logDir -ItemType Directory -ErrorAction SilentlyContinue
-    if (-not (Test-Path $logDir)) {
-        Write-Error "Failed to create log directory at $logDir. Logging will not be possible."
-        return
-    }
+    New-Item -Path $logDir -ItemType Directory | Out-Null
 }
 
-# Enhanced logging function with error handling
+# Logging function
 function Log-Message {
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [string]$Message
     )
     $timestamp = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
     $logEntry = "[$timestamp] $Message"
-    try {
-        Add-Content -Path $logPath -Value $logEntry -ErrorAction Stop
-    } catch {
-        Write-Error "Failed to write to log: $_"
-    }
+    Add-Content -Path $logPath -Value $logEntry
 }
 
-# Example of how to use the Log-Message function within your script
-try {
-    Log-Message "Script starting execution."
-
-    # Script operations go here...
-    
-    Log-Message "Script completed successfully."
-} catch {
-    Log-Message "An error occurred: $_"
-    # Additional error handling logic can go here...
-}
