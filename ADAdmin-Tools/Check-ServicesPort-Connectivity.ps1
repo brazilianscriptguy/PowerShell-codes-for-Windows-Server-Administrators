c6e7037f09dc9d3c90cea030e69b89dc9205972c
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-05-08 16:05:20 -0300

    Update Check-ServicesPort-Connectivity.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/Check-ServicesPort-Connectivity.ps1
+++ ADAdmin-Tools/Check-ServicesPort-Connectivity.ps1
@@ -1,6 +1,29 @@
 # PowerShell Script to Check Services Ports Connectivity
 # Author: Luiz Hamilton Silva - @brazilianscriptguy
-# Update: May 06, 2024.
+# Update: May 8, 2024.
+
+# Hide the PowerShell console window
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+public class Window {
+    [DllImport("kernel32.dll", SetLastError = true)]
+    static extern IntPtr GetConsoleWindow();
+    [DllImport("user32.dll", SetLastError = true)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+    public static void Hide() {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 0); // 0 = SW_HIDE
+    }
+    public static void Show() {
+        var handle = GetConsoleWindow();
+        ShowWindow(handle, 5); // 5 = SW_SHOW
+    }
+}
+"@
+
+[Window]::Hide()
 
 # Load necessary .NET assemblies for GUI
 Add-Type -AssemblyName System.Windows.Forms
