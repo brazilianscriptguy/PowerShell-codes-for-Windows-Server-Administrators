f9fdb5fdc1a6038fcb36cd917416531eca9ccc7d
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-05-09 17:43:22 -0300

    Update Create-ScriptGlobalCSV-Exports.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/Create-ScriptGlobalCSV-Exports.ps1
+++ ADAdmin-Tools/Create-ScriptGlobalCSV-Exports.ps1
@@ -1,5 +1,3 @@
 # Determine the script name for logging and exporting .csv files
 $scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
-
-
 $csvPath = [Environment]::GetFolderPath('MyDocuments') + "\${scriptName}-$dcName-$timestamp.csv"
