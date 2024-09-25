4468c7861ce7868bbc65486232c90c496cb2a4d5
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-09-25 11:22:51 -0300

    Delete ADAdmin-Tools/Create-Script-MainCSV-Exports.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- ADAdmin-Tools/Create-Script-MainCSV-Exports.ps1
+++
@@ -1,3 +0,0 @@
-# Determine the script name for logging and exporting .csv files
-$scriptName = [System.IO.Path]::GetFileNameWithoutExtension($MyInvocation.MyCommand.Name)
-$csvPath = [Environment]::GetFolderPath('MyDocuments') + "\${scriptName}-$dcName-$timestamp.csv"
