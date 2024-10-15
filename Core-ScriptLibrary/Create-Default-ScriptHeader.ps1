9560b61c9151a264057ab8b6a7c9a762415e533a
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-10-15 12:10:29 -0300

    Create Create-Default-ScriptHeader.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

---
+++ Core-ScriptLibrary/Create-Default-ScriptHeader.ps1
@@ -0,0 +1,15 @@
+<#
+.SYNOPSIS
+    PowerShell Script to Clean Up Orphaned and Outdated Domains, Including Metadata and Residual Data.
+
+.DESCRIPTION
+    This script provides a GUI tool for managing Active Directory (AD) domains and domain controllers (DCs). 
+    It allows administrators to clean up orphaned domains, remove specific distinguished names (CNs), 
+    synchronize all DCs, and view detailed logs of operations performed.
+
+.AUTHOR
+    Luiz Hamilton Silva - @brazilianscriptguy
+
+.VERSION
+    Last Updated: August 14, 2024
+#>
