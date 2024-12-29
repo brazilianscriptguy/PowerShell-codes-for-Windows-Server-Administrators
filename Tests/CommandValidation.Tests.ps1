d39b661f90e0f427afc6a563639eebea97e5fc1b
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-12-29 14:06:28 -0300

    Update CommandValidation.Tests.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- Tests/CommandValidation.Tests.ps1
+++ Tests/CommandValidation.Tests.ps1
@@ -1,14 +1,29 @@
 Describe 'Test Get-UserInfo Function' {
     Context 'When valid parameters are passed' {
+        Mock -CommandName Get-UserInfo -MockWith {
+            [PSCustomObject]@{
+                UserName = 'TestUser'
+                FullName = 'Test User'
+                Email    = 'testuser@example.com'
+                Title    = 'System Admin'
+            }
+        }
+
         It 'Should return user information' {
             $Result = Get-UserInfo -UserName 'TestUser'
             $Result | Should -Not -BeNullOrEmpty
+            $Result.UserName | Should -Be 'TestUser'
+            $Result.Email | Should -Be 'testuser@example.com'
         }
     }
 
     Context 'When invalid parameters are passed' {
+        Mock -CommandName Get-UserInfo -MockWith {
+            throw "Invalid username"
+        }
+
         It 'Should throw an error' {
-            { Get-UserInfo -UserName '' } | Should -Throw
+            { Get-UserInfo -UserName '' } | Should -Throw -ErrorMessage "Invalid username"
         }
     }
 }
