f2feeba5a7feda8675a054a1e66ff96dd01a13e7
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-12-29 14:41:02 -0300

    Update CommandValidation.Tests.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- Tests/CommandValidation.Tests.ps1
+++ Tests/CommandValidation.Tests.ps1
@@ -1,30 +1,31 @@
 Describe 'Test Get-UserInfo Function' {
+
     # Mock definition for Get-UserInfo
     Mock -CommandName Get-UserInfo -MockWith {
-        param([string]$UserName)
-        if (-not $UserName -or $UserName -eq '') {
+        param([string]$SamAccountName)
+        if (-not $SamAccountName -or $SamAccountName -eq '') {
             throw [ParameterBindingException]::new("Invalid username provided")
         }
         [PSCustomObject]@{
-            UserName = $UserName
-            FullName = "$UserName Full"
-            Email    = "$UserName@example.com"
-            Title    = "Mocked Title"
+            SamAccountName = $SamAccountName
+            FullName       = "$SamAccountName Full"
+            Email          = "$SamAccountName@example.com"
+            Title          = "Mocked Title"
         }
     }
 
     Context 'When valid parameters are passed' {
         It 'Should return user information' {
-            $Result = Get-UserInfo -UserName 'ValidUser'
+            $Result = Get-UserInfo -SamAccountName 'ValidUser'
             $Result | Should -Not -BeNullOrEmpty
-            $Result.UserName | Should -Be 'ValidUser'
-            $Result.Email | Should -Be 'ValidUser@example.com'
+            $Result.SamAccountName | Should -Be 'ValidUser'
+            $Result.Email         | Should -Be 'ValidUser@example.com'
         }
     }
 
     Context 'When invalid parameters are passed' {
         It 'Should throw an error' {
-            { Get-UserInfo -UserName '' } | Should -Throw -ErrorType 'ParameterBindingException'
+            { Get-UserInfo -SamAccountName '' } | Should -Throw -ErrorType 'ParameterBindingException'
         }
     }
 }
