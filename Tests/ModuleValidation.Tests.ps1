8c113be10179f8cb160fb4b505b54b39aab35c5f
LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>
2024-12-29 14:31:25 -0300

    Update ModuleValidation.Tests.ps1
    Signed-off-by: LUIZ HAMILTON ROBERTO DA SILVA <luizhamilton.lhr@gmail.com>

--- Tests/ModuleValidation.Tests.ps1
+++ Tests/ModuleValidation.Tests.ps1
@@ -1,5 +1,5 @@
 Describe 'Windows-SysAdmin-ProSuite Module Validation' {
-    # Dynamically resolve the paths
+    # Dynamically resolve paths
     $PSScriptRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
     $ManifestPath = (Join-Path -Path $PSScriptRoot -ChildPath '../Windows-SysAdmin-ProSuite.psd1')
     $ModulePath = (Join-Path -Path $PSScriptRoot -ChildPath '../Windows-SysAdmin-ProSuite.psm1')
@@ -12,7 +12,7 @@ Describe 'Windows-SysAdmin-ProSuite Module Validation' {
     It 'Should export all expected commands' {
         Test-Path -Path $ModulePath | Should -BeTrue -Because "The module file should exist at $ModulePath"
         $ExportedCmdlets = (Import-Module -Name $ModulePath -PassThru).ExportedCommands.Keys
-        $ExpectedCmdlets = @('Get-UserInfo', 'AnotherFunction', 'ThirdFunction') # Replace with actual function names
+        $ExpectedCmdlets = @('Get-UserInfo') # Replace with actual function names
         $ExportedCmdlets | Should -ContainEveryItemOf $ExpectedCmdlets -Because "The module should export all expected commands"
     }
 }
